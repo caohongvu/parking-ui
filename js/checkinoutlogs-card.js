6ab84e6647c70a7c97a280d3c4a44ab2572084aa
minhtb1112 <minhtb1112@gmail.com>
2019-12-02 13:46:52 +0700

    Update
    Fix checkinoutlogs-card.js
    Add new forms

--- js/checkinoutlogs-card.js
+++ js/checkinoutlogs-card.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $.get("http://api.spmgroup.vn:8080/parking-apis/orders", function (jsonData) {
         var tableData = $("#inOutTable");
         var rowTemplate = "<tr>" +
-            "<th scope=\"row\">#index</th>" +
             "<td>#id</td>" +
             "<td>#STT</td>" +
             "<td>#cardCode</td>" +
@@ -21,7 +20,7 @@ $(document).ready(function () {
             "</tr>";
         tableData.find("tbody tr").remove();
         $.each(jsonData, function (k, v) {
-            var newRow = rowTemplate.replace("#index", k + 1).replace("#id", v
+            var newRow = rowTemplate.replace("#id", v
                     .order_id).replace("#STT",
                     v.card_stt).replace("#cardCode", v.card_code).replace("#timeIn", v
                     .checkin_time)
